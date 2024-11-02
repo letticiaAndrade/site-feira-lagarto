da4d693d7b019dea62f6af36ad1f7b991cb01bc0
letticiaAndrade <leticiaae@hotmail.com.br>
2024-11-02 17:06:34 -0300

    fix responsive footer

--- site-feira-lagarto/src/components/Footer.jsx
+++ site-feira-lagarto/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import 'leaflet/dist/leaflet.css';
 import { Flex, HStack, Image, Stack, Text } from "@chakra-ui/react";
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
 import Logo from '../assets/logo_site_sem_fundo.png';
 import Github from '../assets/rede_social_icons/GitHub.png';
@@ -8,71 +8,70 @@ import LinkedIn from '../assets/rede_social_icons/LinkedIn.png';
 import Instagram from '../assets/rede_social_icons/Instagram.png';
 
 export function Footer() {
-
-   const latitude = -10.9195371,
-    longitude= -37.6664342,
-    isIOS = () => {
+    const latitude = -10.9195371;
+    const longitude = -37.6664342;
+    const isIOS = () => {
         const userAgent = navigator.userAgent || navigator.vendor || window.opera;
         return /iPad|iPhone|iPod/.test(userAgent) && !window.MSStream;
     };
-   
-    return(
+
+    return (
         <Flex w="full" bgGradient='linear(to-b, primary.300, primary.400)'>
-
-        <Flex w="full" borderWidth={2} borderColor="dark.400" p={10} my={5} mx={20}>
-            
-            <Stack alignItems="baseline">
-            <Image src={Logo} objectFit="contain" w={90} h={90}/>
-                
-            <Flex flexDirection="column" alignItems="baseline">
-            <Text fontWeight="bold">Endereço:</Text>
-            <Text>Praça Paulo Vieira Menezes - Centro, Lagarto - SE, 49400-000</Text>
-            </Flex>
-
-            <Flex flexDirection="column" alignItems="baseline">
-            <Text fontWeight="bold">Contatos:</Text>
-            <Text>(7X) XXXXXXXXX</Text>
-            <Text> info@relume.io</Text>   
-            </Flex>
-
-            <HStack>
-            <Image cursor="pointer" onClick={()=> alert("Github")} src={Github} objectFit="contain" boxSize={22}/>
-            <Image cursor="pointer" onClick={()=> alert("Instagram")} src={Instagram} objectFit="contain" boxSize={22}/>
-            <Image cursor="pointer" onClick={()=> alert("LinkedIn")} src={LinkedIn} objectFit="contain" boxSize={22}/>
-
-            </HStack>
-            </Stack> 
-
-            <Flex justifyContent="flex-end" w='full'>
             <Flex 
-            w="400px" 
-            h="300px" 
-            onClick={()=> window.open(isIOS() ? `http://maps.apple.com/?ll=${latitude},${longitude}`: `https://www.google.com/maps?q=${latitude},${longitude}`)}
+                w="full" 
+                borderWidth={2} 
+                borderColor="dark.400" 
+                p={{ base: 4, md: 10 }} 
+                my={5} 
+                mx={{ base: 2, md: 20 }}
+                flexDirection={{ base: "column", md: "row" }} 
             >
+                <Stack alignItems="baseline" mb={{ base: 4, md: 0 }}> 
+                    <Image src={Logo} objectFit="contain" w={90} h={90} />
 
-            <MapContainer
-                center={[latitude, longitude]}
-                zoom={30}
-                scrollWheelZoom={false}
-                style={{ width: "100%", height: "100%" }}
-            >
+                    <Flex flexDirection="column" alignItems="baseline">
+                        <Text fontWeight="bold">Endereço:</Text>
+                        <Text>Praça Paulo Vieira Menezes - Centro, Lagarto - SE, 49400-000</Text>
+                    </Flex>
 
-            <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            
-            />
-        
-            <Marker position={[latitude, longitude]}>
-            <Popup>
-                Praça Paulo Vieira Menezes, Lagarto - SE
-            </Popup>
-            </Marker>
-        </MapContainer>
-        </Flex>
-        </Flex>
+                    <Flex flexDirection="column" alignItems="baseline">
+                        <Text fontWeight="bold">Contatos:</Text>
+                        <Text>(7X) XXXXXXXXX</Text>
+                        <Text>info@relume.io</Text>
+                    </Flex>
 
+                    <HStack spacing={4} mt={2}>
+                        <Image cursor="pointer" onClick={() => alert("Github")} src={Github} objectFit="contain" boxSize={22} />
+                        <Image cursor="pointer" onClick={() => alert("Instagram")} src={Instagram} objectFit="contain" boxSize={22} />
+                        <Image cursor="pointer" onClick={() => alert("LinkedIn")} src={LinkedIn} objectFit="contain" boxSize={22} />
+                    </HStack>
+                </Stack>
+
+                <Flex justifyContent="flex-end" w='full'>
+                    <Flex
+                        w={{ base: "100%", md: "400px" }}
+                        h={{ base: "200px", md: "300px" }} 
+                        onClick={() => window.open(isIOS() ? `http://maps.apple.com/?ll=${latitude},${longitude}` : `https://www.google.com/maps?q=${latitude},${longitude}`)}
+                    >
+                        <MapContainer
+                            center={[latitude, longitude]}
+                            zoom={30}
+                            scrollWheelZoom={false}
+                            style={{ width: "100%", height: "100%" }}
+                        >
+                            <TileLayer
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            />
+                            <Marker position={[latitude, longitude]}>
+                                <Popup>
+                                    Praça Paulo Vieira Menezes, Lagarto - SE
+                                </Popup>
+                            </Marker>
+                        </MapContainer>
+                    </Flex>
+                </Flex>
+            </Flex>
         </Flex>
-        </Flex>
-    )
+    );
 }
