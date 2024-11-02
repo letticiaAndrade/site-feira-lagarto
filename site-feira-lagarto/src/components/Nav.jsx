f8cb372296e71b1fb791db3d6276220ef4d04c63
letticiaAndrade <leticiaae@hotmail.com.br>
2024-11-02 17:02:11 -0300

    fix responsive

--- site-feira-lagarto/src/components/Nav.jsx
+++ site-feira-lagarto/src/components/Nav.jsx
@@ -1,59 +1,118 @@
 import { useLocation } from 'react-router-dom';
-import {CaretRight} from "@phosphor-icons/react";
-import { Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Flex, Image, Link, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
-
+import { CaretRight, List } from "@phosphor-icons/react";
+import {
+  Box,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Flex,
+  Image,
+  Link,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  IconButton,
+  Drawer,
+  DrawerBody,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure
+} from '@chakra-ui/react';
 import Logo from '../assets/logo_site_sem_fundo.png';
 
 export function Nav() {
-    const location = useLocation();
-    const currentPath = location.pathname;
-    return (
-        <Box w='full' >
-          <Flex borderBottomWidth={2} px={20} borderBottomColor="dark.100" alignItems="center" justifyContent="space-between">
 
-            <Link href="/" color="primary.400" fontWeight="bold" fontSize="xl">
-            <Image src={Logo} objectFit="contain" w={90} h={90}/>
-            </Link>
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-            <Flex alignItems="center">
-            
-              <Breadcrumb spacing='8px' separator={<CaretRight color="primary.400" />}>
+  return (
+    <Box w="full">
+      <Flex
+        borderBottomWidth={2}
+        px={{ base: 4, md: 20 }}
+        borderBottomColor="dark.100"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Link href="/" color="primary.400" fontWeight="bold" fontSize="xl">
+          <Image src={Logo} objectFit="contain" w={90} h={90} />
+        </Link>
+
+        <Flex display={{ base: "none", lg: "flex" }} alignItems="center">
+          <Breadcrumb spacing="8px" separator={<CaretRight color="primary.400" />}>
             <BreadcrumbItem isCurrentPage={currentPath === "/"}>
-         <BreadcrumbLink href="/" color="primary.400">Início</BreadcrumbLink>
-        </BreadcrumbItem>
-  
-        <BreadcrumbItem>
-        <BreadcrumbLink color="primary.400">
-        <Menu >
-                <MenuButton>
-                  Lojistas
-                </MenuButton>
-                <MenuList>
-                  <MenuItem as={Link} href="/lojistas/conveniencia">Conveniência</MenuItem>
-                  <MenuItem as={Link} href="/lojistas/supermercados">Supermercados</MenuItem>
-                  <MenuItem as={Link} href="/lojistas/modaEvestuario">Moda e Vestuário</MenuItem>
-                  <MenuItem as={Link} href="/lojistas/moveisEdecoracao">Móveis e Decoração</MenuItem>
-                  <MenuItem as={Link} href="/lojistas/oficinasMecanicas">Oficinas e Mecânicas</MenuItem>
-                </MenuList>
-              </Menu>
+              <BreadcrumbLink href="/" color="primary.400">Início</BreadcrumbLink>
+            </BreadcrumbItem>
+
+            <BreadcrumbItem>
+              <BreadcrumbLink color="primary.400">
+                <Menu>
+                  <MenuButton>
+                    Lojistas
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem as={Link} href="/lojistas/conveniencia">Conveniência</MenuItem>
+                    <MenuItem as={Link} href="/lojistas/supermercados">Supermercados</MenuItem>
+                    <MenuItem as={Link} href="/lojistas/modaEvestuario">Moda e Vestuário</MenuItem>
+                    <MenuItem as={Link} href="/lojistas/moveisEdecoracao">Móveis e Decoração</MenuItem>
+                    <MenuItem as={Link} href="/lojistas/oficinasMecanicas">Oficinas e Mecânicas</MenuItem>
+                  </MenuList>
+                </Menu>
               </BreadcrumbLink>
-              </BreadcrumbItem>
+            </BreadcrumbItem>
 
-              <BreadcrumbItem isCurrentPage={currentPath === "/culture"}>
-          <BreadcrumbLink  color="primary.400" href='/culture'>Cultura</BreadcrumbLink>
-         </BreadcrumbItem>
+            <BreadcrumbItem isCurrentPage={currentPath === "/culture"}>
+              <BreadcrumbLink color="primary.400" href="/culture">Cultura</BreadcrumbLink>
+            </BreadcrumbItem>
 
-        <BreadcrumbItem isCurrentPage={currentPath === "/services"}>
-          <BreadcrumbLink color="primary.400" href='/services'>Serviços</BreadcrumbLink>
-         </BreadcrumbItem>
+            <BreadcrumbItem isCurrentPage={currentPath === "/services"}>
+              <BreadcrumbLink color="primary.400" href="/services">Serviços</BreadcrumbLink>
+            </BreadcrumbItem>
 
-         <BreadcrumbItem isCurrentPage={currentPath === "/contact"}>
-          <BreadcrumbLink href="/contact" color="primary.400">Contato</BreadcrumbLink>
-         </BreadcrumbItem>
-        </Breadcrumb>
-            </Flex>
-          </Flex>
-        </Box>
-      );
-    }
+            <BreadcrumbItem isCurrentPage={currentPath === "/contact"}>
+              <BreadcrumbLink href="/contact" color="primary.400">Contato</BreadcrumbLink>
+            </BreadcrumbItem>
+          </Breadcrumb>
+        </Flex>
 
+        <IconButton
+          display={{ base: "flex", lg: "none" }}
+          icon={<List size={24} />}
+          onClick={onOpen}
+          variant="ghost"
+          aria-label="Abrir menu"
+        />
+
+        <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerBody mt={8}>
+              <Flex flexDirection="column">
+                <Link href="/" color="primary.400" mb={4} onClick={onClose}>Início</Link>
+                <Menu>
+                  <MenuButton as={Link} color="primary.400" mb={4}>
+                    Lojistas
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem as={Link} href="/lojistas/conveniencia" onClick={onClose}>Conveniência</MenuItem>
+                    <MenuItem as={Link} href="/lojistas/supermercados" onClick={onClose}>Supermercados</MenuItem>
+                    <MenuItem as={Link} href="/lojistas/modaEvestuario" onClick={onClose}>Moda e Vestuário</MenuItem>
+                    <MenuItem as={Link} href="/lojistas/moveisEdecoracao" onClick={onClose}>Móveis e Decoração</MenuItem>
+                    <MenuItem as={Link} href="/lojistas/oficinasMecanicas" onClick={onClose}>Oficinas e Mecânicas</MenuItem>
+                  </MenuList>
+                </Menu>
+                <Link href="/culture" color="primary.400" mb={4} onClick={onClose}>Cultura</Link>
+                <Link href="/services" color="primary.400" mb={4} onClick={onClose}>Serviços</Link>
+                <Link href="/contact" color="primary.400" mb={4} onClick={onClose}>Contato</Link>
+              </Flex>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+      </Flex>
+    </Box>
+  );
+}
--- site-feira-lagarto/src/pages/Contact.jsx
+++ site-feira-lagarto/src/pages/Contact.jsx
@@ -1,20 +1,18 @@
 import { useRef } from "react";
-import {sendForm} from "@emailjs/browser"
+import { sendForm } from "@emailjs/browser";
 import { useForm } from "react-hook-form";
 import { Footer, Nav } from "../components";
 import { Box, Button, Checkbox, Flex, FormControl, FormLabel, Heading, Image, Input, Text, Textarea } from "@chakra-ui/react";
 
 import Image1 from "../assets/imageContact.jpg";
 
-export function Contact () {
+export function Contact() {
 
     const form = useRef();
-
     const { register, handleSubmit, formState: { errors } } = useForm();
 
     const onSubmit = (data) => {
-        console.log(data)
-        // Enviar dados através do EmailJS
+        console.log(data);
         sendForm('service_e26s9fa', 'template_nqufphh', form.current, { 
             publicKey: "wVxWhHkhFWpawfTaB"
         })
@@ -25,28 +23,43 @@ export function Contact () {
             console.log(error.text);
         });
     };
-    
+
     const year = new Date().getFullYear();
 
-    return(
-        <Flex w="100%" flexDirection="column" h="100%" bgGradient='linear(to-b, primary.100, primary.400)' alignItems="center">
-
-            <Nav/>
-            <Flex justify="space-between"
+    return (
+        <Flex
+            w="100%"
+            flexDirection="column"
+            h="100%"
+            bgGradient="linear(to-b, primary.100, primary.400)"
+            alignItems="center"
+            p={4}
+        >
+            <Nav />
+            <Flex
+                justify="center"
                 alignItems="center"
-                flexDirection={{base:"column", sm: "column", md: "column", lg: "row"}} 
-                w={{base: 300, sm: 500, md:800, lg:1730 }}>
-
-                <Flex flexDirection="column" w='full'>
-                    <Heading fontSize={{base:20, sm:28, md:32, lg: 48}} fontWeight="bold">Entre em contato</Heading>
-                    <Box mr={20}>
-
+                flexDirection={{ base: "column", lg: "row" }}
+                w="full"
+                maxW="1200px"
+                px={{ base: 4, md: 8 }}
+            >
+                <Flex flexDirection="column" w="full" maxW={{ lg: "600px" }} p={4}>
+                    <Heading
+                        fontSize={{ base: 24, sm: 32, md: 40, lg: 48 }}
+                        fontWeight="bold"
+                        textAlign={{ base: "center", lg: "left" }}
+                        mb={4}
+                    >
+                        Entre em contato
+                    </Heading>
+                    <Box>
                         <form ref={form} onSubmit={handleSubmit(onSubmit)}>
                             <FormControl isInvalid={errors.name} mb={4}>
                                 <FormLabel htmlFor="name">Nome</FormLabel>
                                 <Input
                                     borderColor="dark.400"
-                                    variant='outline' 
+                                    variant="outline"
                                     id="name"
                                     placeholder="Seu Nome"
                                     {...register("name", { required: "Nome é obrigatório" })}
@@ -57,7 +70,7 @@ export function Contact () {
                             <FormControl isInvalid={errors.email} mb={4}>
                                 <FormLabel htmlFor="email">Email</FormLabel>
                                 <Input
-                                    variant='outline'
+                                    variant="outline"
                                     borderColor="dark.400"
                                     id="email"
                                     type="email"
@@ -77,7 +90,7 @@ export function Contact () {
                                 <FormLabel htmlFor="message">Mensagem</FormLabel>
                                 <Textarea
                                     borderColor="dark.400"
-                                    variant='outline'
+                                    variant="outline"
                                     id="message"
                                     placeholder="Escreva a sua mensagem..."
                                     {...register("message", { required: "Mensagem é obrigatória" })}
@@ -86,7 +99,7 @@ export function Contact () {
                             </FormControl>
 
                             <FormControl isInvalid={errors.acceptTerms} mb={4}>
-                                <Checkbox                                 
+                                <Checkbox
                                     borderColor="dark.400"
                                     id="acceptTerms"
                                     {...register("acceptTerms", { required: "Você deve aceitar os termos para enviar os dados" })}
@@ -96,18 +109,24 @@ export function Contact () {
                                 {errors.acceptTerms && <Text color="red.500">{errors.acceptTerms.message}</Text>}
                             </FormControl>
 
-                            <Button type="submit" colorScheme="dark">Enviar</Button>
+                            <Button type="submit" colorScheme="dark" w="full" mt={4}>
+                                Enviar
+                            </Button>
                         </form>
                     </Box>
                 </Flex>
 
-                <Image src={Image1} w={616} h={734} my={14}/>
+                <Image
+                    src={Image1}
+                    w={{ base: "90%", sm: "70%", md: "50%", lg: "45%" }}
+                    h="auto"
+                    mt={{ base: 6, lg: 0 }}
+                    borderRadius="lg"
+                />
             </Flex>
 
-            <Footer/>
-
-        <Text>© {year} Instituto Federal de Sergipe. All rights reserved.</Text>
-
+            <Footer />
+            <Text mt={4} fontSize="sm">© {year} Instituto Federal de Sergipe. All rights reserved.</Text>
         </Flex>
-    )
+    );
 }
